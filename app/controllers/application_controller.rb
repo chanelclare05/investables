6f276a1be988db4dc8db8eb6b10c90eff7eef358
chanel donahoe <cdonahoe05@gmail.com>
2021-11-08 22:26:45 +1100

    built the users resources using devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  #all methods/macros included in the application controller is accessible by all other controllers
+  before_action :authenticate_user!
 end
