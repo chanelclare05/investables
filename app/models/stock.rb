d588795fa2ebeaaee4d321d133bb92032a0bb882
chanel donahoe <cdonahoe05@gmail.com>
2021-11-09 17:59:57 +1100

    dealing with invaid search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,9 +3,12 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
                                 publishable_token: Rails.application.credentials.iex_client[:api_key],
-                                endpoint: 'https://cloud.iexapis.com/v1'
-    )
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol) )
+                                endpoint: 'https://cloud.iexapis.com/v1')
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol) )
+    rescue => exception
+      return nil
+    end 
   end 
 
 end
