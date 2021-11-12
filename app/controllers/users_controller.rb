88486a448cb0402728a3845021c829d699702d87
chanel donahoe <cdonahoe05@gmail.com>
2021-11-12 12:31:03 +1100

    added table to my portfolio view and created stock list partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    #Use of the devise gem of current_user to grab the current_user
+    @tracked_stocks = current_user.stocks
   end
 end
