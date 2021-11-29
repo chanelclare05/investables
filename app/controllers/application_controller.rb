a81d4c6feff449cd6d1a8b17ff54393c5e218a93
chanel donahoe <cdonahoe05@gmail.com>
2021-11-29 19:19:43 +1100

    accept additional fields in edit action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   #all methods/macros included in the application controller is accessible by all other controllers
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+
 end
