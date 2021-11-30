09e0bc3662adb45652cfa010d7864e784b80789e
chanel donahoe <cdonahoe05@gmail.com>
2021-11-30 14:22:39 +1100

    disabled api connection due to cost

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,9 +5,11 @@ class Stock < ApplicationRecord
   validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
+    #need to update account billing to make this active
     client = IEX::Api::Client.new(
                                 publishable_token: Rails.application.credentials.iex_client[:api_key],
                                 endpoint: 'https://cloud.iexapis.com/v1')
+                              
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol) )
     rescue => exception
